cec093a7772a9b539ab48eb87c3da6073400277e
Valentyn Mirosh <vlnt@ua.fm>
2022-10-19 22:53:40 +0300

    demo2 module

--- recursion/main.hs
+++ recursion/main.hs
@@ -1,11 +1,10 @@
-module Main where
-
-     vlog3 :: Integer -> Integer
-    
+    vlog3 :: Integer -> Integer
+    vlog3 1 = 1 
+    vlog3 2 = 1
     vlog3 n = 1 + vlog3 (div n 3)
 
     main :: IO()
     main = do 
          num <- getLine
          let n = read num :: Integer
-         print ( Demo_v.vlog3 n )
+         print ( vlog3 n )
