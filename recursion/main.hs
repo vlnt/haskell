8e20e5b1741346582aa4f2a731289dc85126d338
Valentyn Mirosh <vlnt@ua.fm>
2022-10-20 21:28:29 +0300

    multiple file compilation

--- recursion/main.hs
+++ recursion/main.hs
@@ -12,7 +12,7 @@
          let n = read num :: Integer
          print ( Rem_v.vlog3 n )
 
-        --  GHC automatically adds Rem_v for compiling and linking since you specified Main.hs on the command line.
-        --  Since you also specified Rem_v.o, this causes Rem_v.o to be linked twice, resulting in many duplicate definitions.
+        --  GHC automatically adds Rem_v for compiling and linking since is specified Main.hs on the command line.
+        --  Since if also specify Rem_v.o, this causes Rem_v.o to be linked twice, resulting in many duplicate definitions.
         --  Use just 
         --     $~ ghc -o ilog3 main.hs
