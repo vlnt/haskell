9b54e35adffc412c97aeabd6d5db872a885c9056
Valentyn Mirosh <vlnt@ua.fm>
2022-10-22 20:04:01 +0300

    infinite input

--- recursion/main.hs
+++ recursion/main.hs
@@ -1,3 +1,4 @@
+    module Main where
 
     import Rec_set
     -- vlog3 :: Integer -> Integer
@@ -13,20 +14,22 @@
 
     
 
-    -- loop :: IO ()
-    -- loop = do
-    --     Rec_set.palindromify <- getLine
-        -- case  of
-        --     "quit"    -> return ()
-        --     otherwise -> loop
+    loop :: IO ()
+    loop = do
+        line <- getLine
+        print(Rec_set.palindromify line)
+        case line of
+            "quit"    -> return ()
+            otherwise -> loop
         --  num <- getLine
         --  let n = read num :: Integer
         --  print ( Rec_set.v  log3 n )
 
    
     main = do
-        line <- getLine
-        print ( Rec_set.palindromify line )
+        loop
+        -- line <- getLine
+        -- print ( Rec_set.palindromify line )
         --Rec_set.palindromify line
         --  GHC automatically adds Rem_v for compiling and linking since is specified Main.hs on the command line.
         --  Since if also specify Rem_v.o, this causes Rem_v.o to be linked twice, resulting in many duplicate definitions.
