2e84ee5b222186abf36ec295a43f70e085d8281d
Valentyn Mirosh <vlnt@ua.fm>
2022-10-27 18:06:42 +0300

    non trivial list builder

--- starter/List_vset.hs
+++ starter/List_vset.hs
@@ -9,3 +9,15 @@ capitalizeS :: String -> String
 capitalizeS = do
      unwords (map cap  (words s))
      where cap w = toUpper (head(take 1 w)):tail w
+
+-- Use recursion and the : operator to build the list.
+--
+-- Examples:
+--   buildList 1 5 2 ==> [1,1,1,1,1,2]
+--   buildList 7 0 3 ==> [3]
+
+buildList :: Int -> Int -> Int -> [Int]
+buildList start count end = do
+    if count > 0 
+        then start:buildList start (count-1) end
+    else (\x xs -> x:xs) end []
