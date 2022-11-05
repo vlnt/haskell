bed2a08bcd3e5a41832d29e5a35c062a66ca813a
Valentyn Mirosh <vlnt@ua.fm>
2022-11-05 21:50:02 +0200

    armean

--- ML/src/LR.hs
+++ ML/src/LR.hs
@@ -6,11 +6,13 @@ module LR where
     --f x = ax+c // y = mx + b
     -- m = (x1 - x2)/(y1 - y2) // b = y - mx
 
-    lineGradient :: (Int, Int) -> (Int ,Int) -> Int
+    lineGradient :: (Fractional a) => (a, a) -> (a, a) -> a
     lineGradient p1 p2 = do
         (fst p1 - fst p2)/(snd p1 - snd p2)
 
-    lineOffset :: Fractional a => (a, a) -> a -> a 
-
+    lineOffset :: (Fractional a) => (a, a) -> a -> a 
     lineOffset p s = do 
         (snd p) / (fst p * s)
+
+    arMean :: Fractional a => [a] -> a
+    arMean ar = sum ar / fromIntegral (length ar)
